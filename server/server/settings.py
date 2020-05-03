e28303654b4344b92d9e301de5136e8cad1fa9ce
Jialu Deng <jialudeng1009@gmail.com>
2020-05-03 11:55:13 -0700

    Debug mode defaulted to off

--- server/server/settings.py
+++ server/server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG_STATUS", True)
+DEBUG = os.environ.get("DEBUG_DEVELOPMENT_MODE", False)
 
 ALLOWED_HOSTS = ['*.herokuapp.com']
 
