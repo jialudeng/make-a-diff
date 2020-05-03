bb9b20caef9a63a071ded0b357e12a90fa0ef3f1
Jialu Deng <jialudeng1009@gmail.com>
2020-05-02 23:44:03 -0700

    added static root in settings.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'api.User'
